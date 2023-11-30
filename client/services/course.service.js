80a9277266fcf8e62d30281576592cd2a8c4e20c
Echo Hsu <gg9641220003@gmail.com>
2023-11-30 15:18:16 +0800

    local setting

--- client/services/course.service.js
+++ client/services/course.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://mern-api-nu-nine.vercel.app/course/";
+const API_URL = "http://localhost:8080/course/";
 let token;
 if (localStorage.getItem("user")) {
   token = JSON.parse(localStorage.getItem("user")).token;
