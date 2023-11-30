d80f9d563d195a5906e7c6618d5d1882d6162fcc
Echo Hsu <gg9641220003@gmail.com>
2023-11-30 15:01:25 +0800

    local

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -78,7 +78,6 @@ function App() {
               currentUser={currentUser}
               setCurrentUser={setCurrentUser}
               cartlist={cartlist}
-              guest={guest}
             />
           }
         >
--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(
   cors({
-    origin: "https://mern-coolschool.vercel.app",
+    origin: "http://localhost:5173",
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
   })
