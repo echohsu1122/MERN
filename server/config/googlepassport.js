2abca8313706bb738df003bf6ebef5c7b19642b0
Echo Hsu <gg9641220003@gmail.com>
2023-12-01 11:34:07 +0800

    google cb direct

--- server/config/googlepassport.js
+++ server/config/googlepassport.js
@@ -23,7 +23,7 @@ module.exports = (passport) => {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:8080/user/auth/google/redirect",
+        callbackURL: "/user/auth/google/redirect",
       },
       async (accessToken, refreshToken, profile, done) => {
         let foundUser = await User.findOne({ email: profile.emails[0].value });
