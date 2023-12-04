b76509a5562f0af821dcf97ebcff6b51db77c215
Echo Hsu <gg9641220003@gmail.com>
2023-12-04 15:48:51 +0800

    Serverless Function has crashed.

--- server/routes/googleRoute.js
+++ server/routes/googleRoute.js
@@ -1,8 +1,4 @@
 const router = require("express").Router();
-const User = require("../models").user;
-
-const registerVaildation = require("../vaildation").registerVaildation;
-const loginVaildation = require("../vaildation").loginVaildation;
 
 const passport = require("passport");
 require("../config/passport")(passport);
