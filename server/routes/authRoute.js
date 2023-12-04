444c432f1967abf0310f06ca977445e7b11fd8f1
Echo Hsu <gg9641220003@gmail.com>
2023-12-04 11:52:46 +0800

    cache-control

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -17,6 +17,7 @@ router.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   console.log("正在接收auth的請求");
   next();
 });
